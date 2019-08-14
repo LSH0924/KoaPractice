7202194f7b373867dcadd024af0cf853ed25b76a
LSH0924 <tngus25866@gmail.com>
2019-08-14 23:53:53 +0900

    18.3.2.1. app.use의 파라미터 ctx, next

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,11 +2,24 @@ const Koa = require("koa");
 const app = new Koa();
 
 // 사용할 미들웨어 등록하기
+app.use((ctx, next) => {
+    console.log(1);
+    // 다음 순번의 미들웨어를 호출
+    next().then(() => {
+        console.log("모든 미들웨어가 끝났습니다.");
+    });
+});
+
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+});
+
 app.use((ctx) => {
     ctx.body = "hello world!";
 });
 
-// 40000번 포트 열기. 연결되면 console.log를 출력한다.
+// 40000번 포트 열기. 연결되면 console.log를 출력
 app.listen(4000, () => {
     console.log("4000번 포트에 연결되었습니다.");
 });
