a9dcae52b8d6f3ded893a9d9a746a1eb429f83b4
LSH0924 <tngus25866@gmail.com>
2019-08-20 23:38:49 +0900

    18.5.5. posts 라우트 생성

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,10 +1,14 @@
 const Router = require("koa-router");
 
+const posts = require("./posts");
+
 const api = new Router();
 
 api.get("/test", (ctx) => {
     ctx.body = "라우트 모듈화 테스트";
 });
+
+api.use("/posts", posts.routes());
 
 // 라우트 내보내기
 module.exports = api;
